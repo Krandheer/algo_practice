4a6bad19364a35b63df92cc978c599d7d398a2de
randheer <randheer.dakshana15@gmail.com>
2020-09-05 17:54:12 +0530

    4

---
+++ distance of 0 in binary matrix form a cell.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+class Solution:
+    def updateMatrix(self, matrix):
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        queue = deque()
+
+        # for reusing original matrix, convert 1 to infinity at initialization
+        for i, row in enumerate(matrix):
+            for j, ele in enumerate(row):
+                if ele:
+                    matrix[i][j] = float('inf')
+                else:
+                    queue.append((i, j))
+
+        directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
+        while queue:
+            for count in range(len(queue)):
+                i, j = queue.popleft()
+
+                for x, y in directions:
+                    row, col = i + x, j + y
+
+                    if -1 < row < m and -1 < col < n and matrix[row][col] > matrix[i][j] + 1:
+                        matrix[row][col] = matrix[i][j] + 1
+                        queue.append((row, col))
+
+        return matrix
